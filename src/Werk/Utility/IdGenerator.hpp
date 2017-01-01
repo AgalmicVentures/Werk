61036e817066caa45ddf0962db0b848d3ae3696c
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-12-31 23:30:49 -0600

    ID generator (closing #10)

---
+++ src/Werk/Utility/IdGenerator.hpp
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include <cstdint>
+
+namespace werk
+{
+
+class IdGenerator
+{
+public:
+
+	IdGenerator(uint64_t start=0, uint64_t step=1) : _nextId(start), _step(step) { }
+
+	uint64_t step() const { return _step; }
+
+	uint64_t nextId() const { return _nextId; }
+
+	uint64_t getNext() {
+		uint64_t id = _nextId;
+		++_nextId;
+		return id;
+	}
+
+private:
+	//Config
+	uint64_t _step;
+
+	//State
+	uint64_t _nextId;
+};
+
+}

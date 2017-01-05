03bac90eda67f90501efe49d5d8637cb4baedc5b
Ian Hutchinson <ihutchinson@agalmicventures.com>
2017-01-04 19:19:41 -0600

    Simple linear regression class (closes #18)

---
+++ src/Werk/Math/SimpleLinearRegression.hpp
@@ -0,0 +1,49 @@
+
+#pragma once
+
+#include <cmath>
+#include <cstdint>
+#include <limits>
+
+#include "SummaryStatistics.hpp"
+
+namespace werk
+{
+
+class SimpleLinearRegression
+{
+public:
+
+	uint64_t count() const { return _x.count(); }
+
+	double covariance() const { return _xy.average() - _x.average() * _y.average(); }
+	double correlation() const {
+		return count() >= 2 ?
+			std::max(-1.0, std::min(1.0, covariance() / std::sqrt(_x.variance() * _y.variance()))) :
+			std::numeric_limits<double>::quiet_NaN();
+	}
+	double beta() const {
+		return count() >= 2 ? covariance() / _x.variance() : std::numeric_limits<double>::quiet_NaN();
+	}
+	double alpha() const {
+		return _y.average() - _x.average() * beta();
+	}
+
+	void sample(double x, double y) {
+		_x.sample(x);
+		_y.sample(y);
+		_xy.sample(x * y);
+	}
+	void reset() {
+		_x.reset();
+		_y.reset();
+		_xy.reset();
+	}
+
+private:
+	SummaryStatistics<double> _x;
+	SummaryStatistics<double> _y;
+	SummaryStatistics<double> _xy;
+};
+
+}
---
+++ src/WerkTest/Math/SimpleLinearRegression.cpp
@@ -0,0 +1,47 @@
+
+#include <boost/test/unit_test.hpp>
+
+#include "Werk/Math/SimpleLinearRegression.hpp"
+
+BOOST_AUTO_TEST_SUITE(SimpleLinearRegressionTest)
+
+BOOST_AUTO_TEST_CASE(TestEmpty)
+{
+	werk::SimpleLinearRegression r;
+    BOOST_REQUIRE_EQUAL(r.count(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(TestBasic)
+{
+    werk::SimpleLinearRegression r;
+
+    r.sample(1.0, 4.0);
+    r.sample(3.0, 8.0);
+    BOOST_REQUIRE_EQUAL(r.count(), 2);
+    BOOST_REQUIRE_EQUAL(r.beta(), 2.0);
+    BOOST_REQUIRE_EQUAL(r.correlation(), 1.0);
+
+    r.sample(5.0, 12.0);
+    BOOST_REQUIRE_EQUAL(r.count(), 3);
+    BOOST_REQUIRE_EQUAL(r.correlation(), 1.0);
+    BOOST_REQUIRE_CLOSE(r.beta(), 2.0, 0.000000001);
+    BOOST_REQUIRE_CLOSE(r.alpha(), 2.0, 0.000000001);
+
+    r.reset();
+    BOOST_REQUIRE_EQUAL(r.count(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(TestZero)
+{
+    werk::SimpleLinearRegression r;
+
+    r.sample(-1.0, 3.0);
+    r.sample(0.0, 0.0);
+    r.sample(1.0, 3.0);
+    BOOST_REQUIRE_EQUAL(r.count(), 3);
+    BOOST_REQUIRE_EQUAL(r.correlation(), 0.0);
+    BOOST_REQUIRE_EQUAL(r.beta(), 0.0);
+    BOOST_REQUIRE_CLOSE(r.alpha(), 2.0, 0.000000001);
+}
+
+BOOST_AUTO_TEST_SUITE_END()

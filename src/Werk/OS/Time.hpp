d4ad023c5b377b487353b2ff5d8a71b2e6b59c40
Ian Hutchinson <ihutchinson@agalmicventures.com>
2017-01-29 17:11:28 -0600

    Cleanup OS time funtions

--- src/Werk/OS/Time.hpp
+++ src/Werk/OS/Time.hpp
@@ -11,28 +11,27 @@ namespace werk {
 
 inline uint64_t epochTime()
 {
-#ifndef __MACH__
-	timespec t;
-	if (clock_gettime(CLOCK_MONOTONIC, &t) < 0) {
-		return 0;
-	}
-
-	return t.tv_sec * 1e9l + t.tv_nsec;
-#else
+#ifdef __MACH__
 	struct timeval t;
-	if (gettimeofday(&t, nullptr) != 0) {
+	if (0 != gettimeofday(&t, nullptr)) {
 		return 0;
 	}
 
 	//Only microsecond resolution, unfortunately
 	return t.tv_sec * 1e9l + t.tv_usec * 1e3l;
+#else
+	timespec t;
+	if (0 != clock_gettime(CLOCK_MONOTONIC, &t)) {
+		return 0;
+	}
+
+	return t.tv_sec * 1e9l + t.tv_nsec;
 #endif
 }
 
-class Clock {
-
+class Clock
+{
 public:
-
 	uint64_t time() const { return _time; }
 	void setTime(uint64_t time) { _time = time; }
 	void setEpochTime() { _time = epochTime(); }

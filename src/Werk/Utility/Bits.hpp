abf4d01a5a46dc0ca0b2afcad107a36de664df4e
Ian Hutchinson <ihutchinson@agalmicventures.com>
2017-05-07 21:39:49 -0500

    Bitwise helpers like bit masks and population count (closing #100)

---
+++ src/Werk/Utility/Bits.hpp
@@ -0,0 +1,68 @@
+
+/*
+ * Copyright (c) 2015-2017 Agalmic Ventures LLC (www.agalmicventures.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "Attributes.hpp"
+
+/**
+ * Generates a bit mask with the first n bits set.
+ */
+template <typename T>
+ALWAYS_INLINE T bitmask(size_t n)
+{
+	return static_cast<T>((1ul << n) - 1);
+}
+
+/**
+ * Count the number of 1 bits in a 64-bit number.
+ */
+static ALWAYS_INLINE
+size_t popCount64(uint64_t value)
+{
+	return (size_t) __builtin_popcountll(value);
+}
+
+/**
+ * Rounds a value up to the nearest power of two. On overflow, return 0.
+ */
+template <typename T>
+ALWAYS_INLINE T roundUpPow2(T value)
+{
+	T v = value;
+	--v;
+	v |= v >> 1;
+	v |= v >> 2;
+	v |= v >> 4;
+	if (sizeof(T) > 1) {
+		v |= v >> 8;
+	}
+	if (sizeof(T) > 2) {
+		v |= v >> 16;
+	}
+	if (sizeof(T) > 4) {
+		v |= v >> 32;
+	}
+	++v;
+	return v;
+}
---
+++ src/WerkTest/Utility/Bits.cpp
@@ -0,0 +1,65 @@
+
+/*
+ * Copyright (c) 2015-2017 Agalmic Ventures LLC (www.agalmicventures.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+#include "Werk/Utility/Bits.hpp"
+
+BOOST_AUTO_TEST_SUITE(BitsTest)
+
+BOOST_AUTO_TEST_CASE(TestBitmask)
+{
+	BOOST_CHECK_EQUAL(bitmask<uint64_t>(0), 0x00);
+	BOOST_CHECK_EQUAL(bitmask<uint64_t>(1), 0x01);
+	BOOST_CHECK_EQUAL(bitmask<uint64_t>(2), 0x03);
+	BOOST_CHECK_EQUAL(bitmask<uint64_t>(3), 0x07);
+	BOOST_CHECK_EQUAL(bitmask<uint64_t>(4), 0x0F);
+	BOOST_CHECK_EQUAL(bitmask<uint64_t>(5), 0x1F);
+}
+
+BOOST_AUTO_TEST_CASE(TestRoundUpPow2)
+{
+	//Simple shifting
+	uint64_t x = 0x0E;
+	uint64_t y = 0x10;
+	for (size_t i = 0; i < 32; ++i) {
+		BOOST_CHECK_EQUAL(roundUpPow2(x), y);
+		x *= 0x10;
+		y *= 0x10;
+	}
+
+	//Types matter
+	BOOST_CHECK_EQUAL(roundUpPow2(0xDEADBEEF), 0);
+	BOOST_CHECK_EQUAL(roundUpPow2<uint32_t>(0xDEADBEEF), 0);
+	BOOST_CHECK_EQUAL(roundUpPow2<uint64_t>(0xDEADBEEF), 0x100000000);
+}
+
+BOOST_AUTO_TEST_CASE(TestPopCount64)
+{
+	for (size_t i = 0; i < 64; ++i) {
+		uint64_t value = bitmask<uint64_t>(i);
+		BOOST_CHECK_EQUAL(popCount64(value), i);
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END()

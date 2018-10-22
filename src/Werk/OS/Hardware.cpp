7b169bdca23d517b74fac97dd19370fbd7761e70
Ian Hutchinson <ihutchinson@agalmicventures.com>
2018-10-22 07:55:52 -0500

    Fix mixed/tabs spaces

--- src/Werk/OS/Hardware.cpp
+++ src/Werk/OS/Hardware.cpp
@@ -24,8 +24,8 @@
 #include "Hardware.hpp"
 
 #ifdef __MACH__
-	#include <sys/types.h>
 	#include <sys/sysctl.h>
+	#include <sys/types.h>
 #endif
 #include <unistd.h>
 
@@ -34,34 +34,34 @@ namespace werk
 
 uint64_t getPageSize()
 {
-    const long pageSize = sysconf(_SC_PAGE_SIZE);
-    if (pageSize < 0) {
-        return 0;
-    }
+	const long pageSize = sysconf(_SC_PAGE_SIZE);
+	if (pageSize < 0) {
+		return 0;
+	}
 
-    return static_cast<uint64_t>(pageSize);
+	return static_cast<uint64_t>(pageSize);
 }
 
 uint64_t getPhysicalMemorySize()
 {
 #ifdef __MACH__
 	int name[2] = { CTL_HW, HW_MEMSIZE };
-    u_int nameLength = sizeof(name) / sizeof(name[0]);
-    uint64_t size;
+	u_int nameLength = sizeof(name) / sizeof(name[0]);
+	uint64_t size;
 	size_t sizeLength = sizeof(size);
-    if (sysctl(name, nameLength, &size, &sizeLength, nullptr, 0) < 0) {
-        return 0;
-    }
+	if (sysctl(name, nameLength, &size, &sizeLength, nullptr, 0) < 0) {
+		return 0;
+	}
 
 	return size;
 #else
-    const long pages = sysconf(_SC_PHYS_PAGES);
-    if (pages < 0) {
-        return 0;
-    }
+	const long pages = sysconf(_SC_PHYS_PAGES);
+	if (pages < 0) {
+		return 0;
+	}
 
-    const uint64_t pageSize = getPageSize();
-    return pages * pageSize;
+	const uint64_t pageSize = getPageSize();
+	return pages * pageSize;
 #endif
 }
 

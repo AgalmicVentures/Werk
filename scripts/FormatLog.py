feedc3be539900d3102b6776723b1a73e29e5795
Ian Hutchinson <ihutchinson@agalmicventures.com>
2020-01-01 13:13:14 -0600

    Update copyrights to 2020

--- scripts/FormatLog.py
+++ scripts/FormatLog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2015-2019 Agalmic Ventures LLC (www.agalmicventures.com)
+# Copyright (c) 2015-2020 Agalmic Ventures LLC (www.agalmicventures.com)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
